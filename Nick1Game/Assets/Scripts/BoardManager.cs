af56f0dccaf8585f508f9bc6b55cae13244b676a
Nick Werblun <nickwerblun@gmail.com>
2018-12-25 19:39:03 -0800

    Lasers kind of working

---
+++ Nick1Game/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;       //Allows us to use Lists.
+using Random = UnityEngine.Random;      //Tells Random to use the Unity Engine random number generator.
+
+
+public class BoardManager : MonoBehaviour
+{
+    // Using Serializable allows us to embed a class with sub properties in the inspector.
+    [Serializable]
+    public class Count
+    {
+        public int minimum;             //Minimum value for our Count class.
+        public int maximum;             //Maximum value for our Count class.
+
+
+        //Assignment constructor.
+        public Count(int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+    }
+
+
+    public int columns = 8;                                         //Number of columns in our game board.
+    public int rows = 8;                                            //Number of rows in our game board.
+    public GameObject floorTile;                                 //Array of floor prefabs.
+    public GameObject wallTile;                                  //Array of wall prefabs.
+
+    private Transform boardHolder;                                  //A variable to store a reference to the transform of our Board object.
+    private List<Vector3> gridPositions = new List<Vector3>();   //A list of possible locations to place tiles.
+
+   
+
+
+    //Clears our list gridPositions and prepares it to generate a new board.
+    void InitialiseList()
+    {
+        //Clear our list gridPositions.
+
+            //Loop through x axis (columns).
+            for (int x = 1; x < columns - 1; x++)
+            {
+                //Within each column, loop through y axis (rows).
+                for (int y = 1; y < rows - 1; y++)
+                {
+                //At each index add a new Vector3 to our list with the x and y coordinates of that position.
+                    gridPositions.Add(new Vector3(x, y, 0f));
+                }
+            }
+    }
+
+
+    //Sets up the outer walls and floor (background) of the game board.
+    void BoardSetup()
+    {
+        //Instantiate Board and set boardHolder to its transform.
+        boardHolder = new GameObject("Board").transform;
+        Vector3 floorTileSize = floorTile.GetComponent<Renderer>().bounds.size;
+        Vector3 wallTileSize = wallTile.GetComponent<Renderer>().bounds.size;
+
+        //Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
+        for (int x = -1; x < columns + 1; x++)
+        {
+            //Loop along y axis, starting from -1 to place floor or outerwall tiles.
+            for (int y = -1; y < rows + 1; y++)
+            {
+                //Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
+                GameObject toInstantiate = floorTile;
+                Vector3 coord = new Vector3(x * floorTileSize.x, y * floorTileSize.y, 0f);
+                //Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
+                if (x == -1 || x == columns || y == -1 || y == rows)
+                {
+                    toInstantiate = wallTile;
+                    coord = new Vector3(x * wallTileSize.x, y * wallTileSize.y, 0f);
+                }
+
+                //Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
+                GameObject instance =
+                    Instantiate(toInstantiate, coord, Quaternion.identity) as GameObject;
+
+                //Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+ 
+
+
+    //SetupScene initializes our level and calls the previous functions to lay out the game board
+    public void SetupScene()
+    {
+        //Creates the outer walls and floor.
+        BoardSetup();
+
+        //Reset our list of gridpositions.
+        InitialiseList();
+
+    }
+}
---
+++ Nick1Game/Assets/Scripts/LaserDrawer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserDrawer : MonoBehaviour
+{
+    private LineRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = gameObject.GetComponent<LineRenderer>();
+        renderer.startWidth = 0.033f;
+        renderer.endWidth = 0.033f;
+        renderer.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        renderer.positionCount = 2;
+        Vector3 currMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 currPlayerPos = transform.position;
+        Vector3 clampedMousePos = new Vector3(100*currMousePos.x, 100*currMousePos.y, currMousePos.z);
+        renderer.SetPosition(0, currPlayerPos);
+        //Layers are a 32 bit bitmask. Layers with 1 are on for raycasting, 0 are off. 000001 would check only layer 0 for raycasts.
+        //Layer 9 has the player so we want all layers EXCEPT PlayerLayer
+        int layerMask = ~(1 << LayerMask.NameToLayer("PlayerLayer"));
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)currPlayerPos, (Vector2)currMousePos,
+            Mathf.Infinity, layerMask, -Mathf.Infinity, Mathf.Infinity);
+        if (hit.collider != null)
+            renderer.SetPosition(1, hit.point);
+        else
+            renderer.SetPosition(1, currMousePos);
+    }
+}
